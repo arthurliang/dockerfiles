ff01d1a747a4d62717e0696580dd9688c25ba2fe
Arthur Liang <arthurliang5@gmail.com>
2016-08-22 09:39:59 +0800

    modify init.sh

--- shadowsocksr/init.sh
+++ shadowsocksr/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SSR_PORT=${SSR_PORT:-8888}
 SSR_PASS=${SSR_PASS:-mpswd}
-SSR_METH=${SSR_PASS:-aes-256-cfb}
+SSR_METH=${SSR_METH:-aes-256-cfb}
 SSR_PROTO=${SSR_PROTO:-auth_sha1_v2}
 SSR_OBFS=${SSR_OBFS:-http_simple}
 SSR_OBFPA=${SSR_OBFPA:-baidu.com}
