2d0e4d25f75191ba91a9485f6248b351bc46ceab
Arthur Liang <arthurliang5@gmail.com>
2017-06-15 09:16:32 +0800

    copy source files into container for using

--- meteordevel/init.sh
+++ meteordevel/init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 #
+
+rsync -av --exclude ".git" --exclude "/app/.meteor/local" --exclude "/app/node_modules" /app /home/meteorapp/
+cd /home/meteorapp/app
+
 meteor npm set registry https://registry.npm.taobao.org
 meteor npm set disturl https://npm.taobao.org/dist
 meteor npm set phantomjs_cdnurl http://cdn.npm.taobao.org/dist/phantomjs
