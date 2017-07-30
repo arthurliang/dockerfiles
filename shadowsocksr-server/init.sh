ea5ba4bc881466ccd135415d0ba38af9ccf3676b
Arthur Liang <arthurliang5@gmail.com>
2017-07-30 10:04:10 +0800

    change folder for server

--- shadowsocksr-server/init.sh
+++ shadowsocksr-server/init.sh
@@ -8,13 +8,13 @@ SSR_PROTO=${SSR_PROTO:-auth_sha1_v2}
 SSR_OBFS=${SSR_OBFS:-http_simple}
 SSR_OBFPA=${SSR_OBFPA:-baidu.com}
 
-sed -i "s/0.0.0.0/$SSR_SERVER/g" /opt/shadowsocks/user-config.json
-sed -i "s/8888/$SSR_SV_PORT/g" /opt/shadowsocks/user-config.json
-sed -i "s/1080/$SSR_CL_PORT/g" /opt/shadowsocks/user-config.json
-sed -i "s/mpswd/$SSR_PASS/g" /opt/shadowsocks/user-config.json
-sed -i "s/aes-256-cfb/$SSR_METH/g" /opt/shadowsocks/user-config.json
-sed -i "s/auth_sha1_v2/$SSR_PROTO/g" /opt/shadowsocks/user-config.json
-sed -i "s/http_simple/$SSR_OBFS/g" /opt/shadowsocks/user-config.json
-sed -i "s/baidu.com/$SSR_OBFPA/g" /opt/shadowsocks/user-config.json
+sed -i "s/0.0.0.0/$SSR_SERVER/g" /opt/shadowsocksr/user-config.json
+sed -i "s/8888/$SSR_SV_PORT/g" /opt/shadowsocksr/user-config.json
+sed -i "s/1080/$SSR_CL_PORT/g" /opt/shadowsocksr/user-config.json
+sed -i "s/mpswd/$SSR_PASS/g" /opt/shadowsocksr/user-config.json
+sed -i "s/aes-256-cfb/$SSR_METH/g" /opt/shadowsocksr/user-config.json
+sed -i "s/auth_sha1_v2/$SSR_PROTO/g" /opt/shadowsocksr/user-config.json
+sed -i "s/http_simple/$SSR_OBFS/g" /opt/shadowsocksr/user-config.json
+sed -i "s/baidu.com/$SSR_OBFPA/g" /opt/shadowsocksr/user-config.json
 
 exec "$@"
